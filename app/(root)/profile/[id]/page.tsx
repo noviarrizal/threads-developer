1580a26bd550524ba8bda6e0bf9dc616b2054ce3
arifnoviarrizal <arifnoviarrizal@gmail.com>
2023-08-21 12:55:13 +0700

    creating comment functionality

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = async () => {
+  return (
+    <section>Profile</section>
+  )
+}
+
+export default Page
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -69,7 +69,7 @@ function PostThread({ userId }: { userId: string }) {
               <FormLabel className="text-base-semibold text-light-2">
                 Content
               </FormLabel>
-              <FormControl className="no-focus border bprder-darl-4 bg-dark-3 text-light-1">
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
                 <Textarea rows={15} {...field} />
               </FormControl>
               <FormMessage />
